649077d47803d1c06414d3e424520a79e7866b7d
Igor Perzic <igor.perzic@pspdfkit.com>
2023-05-17 14:06:47 +0200

    change nextjs config for redirects and image patterns

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/*',
+      },
+    ],
+  },
+  redirects: () => [
+    {
+      source: '/',
+      destination: '/home',
+      permanent: true,
+    },
+  ],
 };
 
 module.exports = nextConfig;
