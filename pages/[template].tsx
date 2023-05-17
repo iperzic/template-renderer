0e204abca9134313826f4d76641f62d85c1d77d0
Igor Perzic <igor.perzic@pspdfkit.com>
2023-05-17 21:57:36 +0200

    consume host from environment

--- pages/[template].tsx
+++ pages/[template].tsx
@@ -33,9 +33,7 @@ export async function getStaticProps({
   params,
 }: GetStaticPropsContext): Promise<GetStaticPropsResult<TemplatePageProps>> {
   const { template } = params as Params;
-  const response = await fetch(
-    'https://adapptive-sandbox.bettywebblocks.com/' + template
-  );
+  const response = await fetch(process.env.HOST_URL + template);
 
   if (!response.ok) {
     return {
